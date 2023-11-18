91c940d5fc4d87ccf5dddeb4174b1e18855ca7e8
Dmitry0x0 <volov.dmit@yandex.ru>
2023-11-18 20:30:00 +0300

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# IPFS Node examples
+
+A repository with an example of using the [ipfs-node](https://github.com/0xdino/ipfs-node) library.
+
+Run to view the results:
+
+```bash
+yarn install
+yarn start
+```
---
+++ index.ts
@@ -0,0 +1,31 @@
+import RunIpfsNode, { IpfsNode } from "ipfs-node";
+
+const start = async () => {
+  const ipfsNode: IpfsNode = await RunIpfsNode.run();
+
+  const cid = await ipfsNode.push(
+    Buffer.from(new TextEncoder().encode("Hello world!"))
+  );
+  console.log("> Added file:", cid.toString());
+
+  const res = await ipfsNode.fetch(cid);
+  console.log("> Added file contents:", new TextDecoder().decode(res));
+
+  const peerid = ipfsNode.node.libp2p.peerId;
+  console.log("> PeerID:", peerid);
+
+  return ipfsNode.node;
+};
+
+start()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .then((node) => {
+    node.stop();
+    console.log(
+      "> The execution of the example is completed, the node has been successfully stopped."
+    );
+    process.exit(0);
+  });
