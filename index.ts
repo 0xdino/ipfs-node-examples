328d88ca25d9979f201a2cfcffc796adeacb7dbc
Dmitry0x0 <volov.dmit@yandex.ru>
2024-01-07 19:56:30 +0300

    fix: test

--- index.ts
+++ index.ts
@@ -3,10 +3,13 @@ import RunIpfsNode, { IpfsNode } from "ipfs-node";
 const start = async () => {
   const ipfsNode: IpfsNode = await RunIpfsNode.run();
 
+  const random =
+    new Date().getTime().toString() + (Math.random() * 2 ** 64).toString(16);
+
   const cid = await ipfsNode.push(
-    Buffer.from(new TextEncoder().encode("Hello world!"))
+    Buffer.from(new TextEncoder().encode(random))
   );
-  console.log("> Added file:", cid.toString());
+  console.log("> Added file:", cid.toString(), random);
 
   const res: Buffer = await ipfsNode.fetch(cid);
   console.log("> Added file contents:", new TextDecoder().decode(res));
